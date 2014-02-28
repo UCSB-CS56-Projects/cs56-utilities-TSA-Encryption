457f34e7f251ed054283e5741ce208fbd8ca3060
Rishabh Jha <rishabh.jha@gmail.com>
2014-02-27 16:12:17 -0800

    GUI partially finished, testing errors with startup

--- src/edu/ucsb/cs56/projects/utilities/TSA_Encryption/CryptographyGUI.java
+++ src/edu/ucsb/cs56/projects/utilities/TSA_Encryption/CryptographyGUI.java
@@ -15,7 +15,7 @@ Cryptography GUI only main method
 
 public class CryptographyGUI{
     JFrame frame;
-
+    JTextField textField;
 	
     /**
        The encrypt function receives an array of integers and transforms it into a long value.
@@ -23,7 +23,7 @@ public class CryptographyGUI{
        @param numlist an int[] (list on integers) that gets iterated through throughout the function, the integers must be greater than or equal to 1 and less than or equal to 1000, and the length of the list must be greater than or equal to 2 and less than or equal to 50.
      */
     
-	public long encrypt1(int[] numList) {
+	public static long encrypt1(int[] numList) {
 	    long totalCheck = 0;
 	    long total = 1;
 	    
@@ -63,7 +63,7 @@ public class CryptographyGUI{
        @param numlist an int[] (list on integers) that gets iterated through throughout the function, the integers must be greater than or equal to -50 and less than or equal to 50, and the length of the list must be greater than or equal to 2 and less than or equal to 10.
      */
 	
-	public long encrypt2(int[] numList){
+	public static long encrypt2(int[] numList){
 	    long totalCheck = 0;
 	    long total = 1;
 	    
@@ -97,7 +97,7 @@ public class CryptographyGUI{
        @return a long with the resulting value
        @param numlist an int[] (list on integers) that gets iterated through throughout the function, the integers must be greater than or equal to -1000 and less than or equal to 1000, and the length of the list must be greater than or equal to 2 and less than or equal to 10.
      */
-	public long encrypt3(int[] numList){
+	public static long encrypt3(int[] numList){
 	    long totalCheck = 0;
 	    long total = 1;
 
@@ -135,35 +135,48 @@ public class CryptographyGUI{
     //all of our inner classes
 
 
-    class Cryptography1Listener implements ActionListener{
+    class Method1Listener implements ActionListener{
 	public void actionPerformed(ActionEvent event){
-	    JFrame frameMethod1 = new JFrame();
-	    frameMethod1.setSize(500,320);
-	    frameMethod1.setTitle("Encryption Method 1");
-	    frameMethod1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    frameMethod1.setVisible(true);
 	    
+	    JPanel allPanel = new JPanel();
+
 	    JTextField textField = new JTextField(20);
 	    JButton encrypt = new JButton("Encrypt");
 
 
 	    JPanel panel1 = new JPanel();
 	    JPanel panel2 = new JPanel();
-	    JPanel allPanel = new JPanel();
 	    allPanel.setLayout(new BoxLayout(allPanel,BoxLayout.Y_AXIS));
 
 	    JPanel title = new JPanel();
 	    JLabel pageTitle = new JLabel("Input a list of integers to encrypt (separated by comma's)");
 	    title.add(pageTitle);
 
+	    JPanel length = new JPanel();
+	    JLabel lengthRestriction = new JLabel("Length of list between 2 and 50");
+	    length.add(lengthRestriction);
+	    
 	    panel1.add(textField);
 	    panel2.add(encrypt);
 
 	    allPanel.add(pageTitle);
+	    allPanel.add(lengthRestriction);
 	    allPanel.add(panel1);
 	    allPanel.add(panel2);
+	    
+	    frame.getContentPane().removeAll();
+	    frame.getContentPane().add(allPanel);
+	    frame.invalidate();
+	    frame.validate();
 
-	    frameMethod1.getContentPane().add(allPanel);
+	    
+	    JButton newButton = new JButton("Encrypt");
+	    newButton.addActionListener(new Encrypt1Listener());
+
+	    
+
+	    frame.invalidate();
+	    frame.validate();
 	}	
 
     }//end of crypt 1 inner class
@@ -171,13 +184,10 @@ public class CryptographyGUI{
 
 
 
-    class Cryptography2Listener implements ActionListener{
+    class Method2Listener implements ActionListener{
 	public void actionPerformed(ActionEvent event){
-	    JFrame frameMethod2 = new JFrame();
-	    frameMethod2.setSize(500,320);
-	    frameMethod2.setTitle("Encryption Method 2");
-	    frameMethod2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    frameMethod2.setVisible(true);
+
+	    JPanel allPanel = new JPanel();
 
 	    JTextField textField = new JTextField(20);
 	    JButton encrypt = new JButton("Encrypt");
@@ -185,21 +195,37 @@ public class CryptographyGUI{
 
 	    JPanel panel1 = new JPanel();
 	    JPanel panel2 = new JPanel();
-	    JPanel allPanel = new JPanel();
 	    allPanel.setLayout(new BoxLayout(allPanel,BoxLayout.Y_AXIS));
 
 	    JPanel title = new JPanel();
 	    JLabel pageTitle = new JLabel("Input a list of integers to encrypt (separated by comma's)");
-
 	    title.add(pageTitle);
+
+	    JPanel length = new JPanel();
+	    JLabel lengthRestriction = new JLabel("Length of list between 2 and 50");
+	    length.add(lengthRestriction);
+
 	    panel1.add(textField);
 	    panel2.add(encrypt);
 
 	    allPanel.add(pageTitle);
+	    allPanel.add(lengthRestriction);
 	    allPanel.add(panel1);
 	    allPanel.add(panel2);
 
-	    frameMethod2.getContentPane().add(allPanel);
+	    frame.getContentPane().removeAll();
+	    frame.getContentPane().add(allPanel);
+	    frame.invalidate();
+	    frame.validate();
+
+
+	    JButton newButton = new JButton("Encrypt");
+	    newButton.addActionListener(new Encrypt2Listener());
+	    
+	    frame.invalidate();
+	    frame.validate();
+
+
 	}
 	
 
@@ -207,13 +233,10 @@ public class CryptographyGUI{
     
 
 
-    class Cryptography3Listener implements ActionListener{
+    class Method3Listener implements ActionListener{
 	public void actionPerformed(ActionEvent event){
-	    JFrame frameMethod3 = new JFrame();
-	    frameMethod3.setSize(500,320);
-	    frameMethod3.setTitle("Encryption Method 3");
-	    frameMethod3.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    frameMethod3.setVisible(true);
+
+	    JPanel allPanel = new JPanel();
 
 	    JTextField textField = new JTextField(20);
 	    JButton encrypt = new JButton("Encrypt");
@@ -221,37 +244,135 @@ public class CryptographyGUI{
 
 	    JPanel panel1 = new JPanel();
 	    JPanel panel2 = new JPanel();
-	    JPanel allPanel = new JPanel();
 	    allPanel.setLayout(new BoxLayout(allPanel,BoxLayout.Y_AXIS));
 	
 	    JPanel title = new JPanel();
 	    JLabel pageTitle = new JLabel("Input a list of integers to encrypt (separated by comma's)");
 	    title.add(pageTitle);
 
+	    JPanel length = new JPanel();
+	    JLabel lengthRestriction = new JLabel("Length of list between 2 and 50");
+	    length.add(lengthRestriction);
+
 	    
 	    panel1.add(textField);
 	    panel2.add(encrypt);
 
 	    allPanel.add(pageTitle);
+	    allPanel.add(lengthRestriction);
 	    allPanel.add(panel1);
 	    allPanel.add(panel2);
 
-	    frameMethod3.getContentPane().add(allPanel);
+	    frame.getContentPane().removeAll();
+	    frame.getContentPane().add(allPanel);
+	    frame.invalidate();
+	    frame.validate();
+
+
+	    JButton newButton = new JButton("Encrypt");
+	    newButton.addActionListener(new Encrypt3Listener());
+	    
+	    frame.invalidate();
+	    frame.validate();
+
+
 	}
     }//end of cryptography3 inner class
     
+
+
+
+
+    class Encrypt1Listener implements ActionListener{
+	public void actionPerformed(ActionEvent event){
+     	    String x = textField.getText();
+
+     	    int[] intArray = stringToIntArray(x);
+
+     	    long encryptedValue = encrypt1(intArray);
+	    
+	    
+     	    JLabel encryptedLabel = new JLabel(Long.toString(encryptedValue));
+     	    JPanel encryptedPanel = new JPanel();
+
+     	    encryptedPanel.add(encryptedLabel);
+
+	    frame.add(encryptedPanel);
+	    	    
+     	}
+	
+    }// end of Encrypt1Listener
     
+    class Encrypt2Listener implements ActionListener{
+	public void actionPerformed(ActionEvent event){
+     	    String x = textField.getText();
+
+     	    int[] intArray = stringToIntArray(x);
+
+     	    long encryptedValue = encrypt2(intArray);
+	    
+     	    JLabel encryptedLabel = new JLabel(Long.toString(encryptedValue));
+     	    JPanel encryptedPanel = new JPanel();
+
+     	    encryptedPanel.add(encryptedLabel);
+     	    
+	    frame.add(encryptedPanel);
+	    	    
+     	}
+	
+    }// end of Encrypt2Listener
+
+ class Encrypt3Listener implements ActionListener{
+	public void actionPerformed(ActionEvent event){
+     	    String x = textField.getText();
+
+     	    int[] intArray = stringToIntArray(x);
+
+     	    long encryptedValue = encrypt3(intArray);
+	    
+     	    JLabel encryptedLabel = new JLabel(Long.toString(encryptedValue));
+     	    JPanel encryptedPanel = new JPanel();
+
+     	    encryptedPanel.add(encryptedLabel);
+     
+	    frame.add(encryptedPanel);
+	    
+	    
+     	}
+	
+    }// end of Encrypt3Listener
+
+
+    //reads input and puts it into an array to intput into encrypt methods
+    public int[] stringToIntArray(String input){
+	String[] StringArray = input.split(",");
+	int[] intArray = new int[StringArray.length];
+
+	int i = 0;
+	for(String intString:StringArray)
+	    {
+		 int x = Integer.parseInt(intString);
+		 intArray[i] = x;
+		 i++;
+	    }
+	return intArray;
+    }// end of stringToIntArray
+		
+	    
+	
 	
 
 
     public static void main(String[] args){
 	CryptographyGUI gui = new CryptographyGUI();
+
 	gui.go();
 
     }//end of main
 
     public void go(){
 	frame = new JFrame();
+	textField = new JTextField();
 
 	frame.setSize(640,480);
 	frame.setTitle("Encryption");
@@ -263,15 +384,14 @@ public class CryptographyGUI{
 	title.add(pageTitle);
 	
 	
-
 	JButton button1 = new JButton("method 1");
-	button1.addActionListener(new Cryptography1Listener());
+	button1.addActionListener(new Method1Listener());
 
 	JButton button2 = new JButton("method 2");
-	button2.addActionListener(new Cryptography2Listener());
+	button2.addActionListener(new Method2Listener());
 	
 	JButton button3 = new JButton("method 3");
-	button3.addActionListener(new Cryptography3Listener());
+	button3.addActionListener(new Method3Listener());
 	
 	JPanel panel1 = new JPanel();
 	JPanel panel2 = new JPanel();
